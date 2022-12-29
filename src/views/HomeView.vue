fff7f3db5c57d846f57be612e96ec16335d8079f
wilker818 <wilkerferreira818@gmail.com>
2022-12-29 13:30:19 -0300

    chore(home): adjusted buttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <BsModal
-    id="ModalPutAuthor"
+    id="ModalPutBook"
     size="modal-lg"
     :opened="modalOpened"
     :showFooter="false"
@@ -22,29 +22,31 @@
               alt="{{ livro.titulo }}"
             />
             <div class="card-body">
-              <h5 class="card-title">Titulo: {{ livro.titulo }}</h5>
+              <h5 class="card-title">{{ livro.titulo }}</h5>
               <p class="card-text">DESCRIÇÃO DO LIVRO</p>
             </div>
             <ul class="list-group list-group-flush">
               <li class="list-group-item">
-                <RouterLink to="/autores"
-                  >Autor(a): {{ livro.autor.nome }}</RouterLink
-                >
+                <RouterLink to="/autores">
+                  Autor(a): {{ livro.autor.nome }}
+                </RouterLink>
               </li>
               <li class="list-group-item">Editora: {{ livro.editora }}</li>
               <li class="list-group-item">
                 Número de páginas: {{ livro.numeroPaginas }}
               </li>
             </ul>
-            <div class="card-body">
+            <div class="card-body d-flex justify-content-between">
               <RouterLink
                 :to="{ name: 'livros', params: { id: livro._id } }"
-                class="card-link"
+                class="btn btn-success"
               >
                 VER LIVRO
               </RouterLink>
 
-              <button class="card-link" @click="modalOpened = true">ALTERAR LIVRO</button>
+              <button class="btn btn-warning" @click="modalOpened = true">
+                ALTERAR LIVRO
+              </button>
             </div>
           </div>
         </div>
@@ -61,7 +63,6 @@ import { ref, onMounted, type Ref } from "vue";
 
 import BsModal from "@/components/BsModal.vue";
 import ModalPutBook from "./components/ModalPutBook.vue";
-
 
 import LivrosApi, { type Livros } from "@/api/livros";
 
