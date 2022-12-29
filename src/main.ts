4d40567e34b7b3772ccb3c6cb7760b921a022186
wilker818 <wilkerferreira818@gmail.com>
2022-12-29 06:39:11 -0300

    feat(bootstrap): added bootstrap and adjusted types bootstrap for typescript

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
-import "bootstrap";
 
 import "@/assets/style/global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
