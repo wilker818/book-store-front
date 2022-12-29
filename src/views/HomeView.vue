14b8b88e09edd95965fcfb612431d78c2ab0fac3
wilker818 <wilkerferreira818@gmail.com>
2022-12-29 10:49:53 -0300

    feat(modal/modal-put-book): added modal to change book information

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,84 @@
 <template>
-  <div class="bs-content container mb-5">
+  <BsModal
+    id="ModalPutAuthor"
+    size="modal-lg"
+    :opened="modalOpened"
+    :showFooter="false"
+    @onclose="modalClose"
+  >
+    <template v-slot:body>
+      <ModalPutBook />
+    </template>
+  </BsModal>
+  <div class="bs-content container d-flex flex-column mb-5">
     <div>Livraria BOOKSTORE</div>
     <div>
       <div class="row">
         <div class="col-md-4 mb-4" v-for="livro in livros" :key="livro._id">
-          <BsItemHome :livros="livro" />
+          <div class="card w-100" style="width: 18rem">
+            <img
+              src="https://dummyimage.com/600x400/000/fff"
+              class="card-img-top"
+              alt="{{ livro.titulo }}"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Titulo: {{ livro.titulo }}</h5>
+              <p class="card-text">DESCRIÇÃO DO LIVRO</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">
+                <RouterLink to="/autores"
+                  >Autor(a): {{ livro.autor.nome }}</RouterLink
+                >
+              </li>
+              <li class="list-group-item">Editora: {{ livro.editora }}</li>
+              <li class="list-group-item">
+                Número de páginas: {{ livro.numeroPaginas }}
+              </li>
+            </ul>
+            <div class="card-body">
+              <RouterLink
+                :to="{ name: 'livros', params: { id: livro._id } }"
+                class="card-link"
+              >
+                VER LIVRO
+              </RouterLink>
+
+              <button class="card-link" @click="modalOpened = true">ALTERAR LIVRO</button>
+            </div>
+          </div>
         </div>
       </div>
+    </div>
+    <div>
+      <button type="button" class="btn btn-success">Adicionar Livro</button>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, onMounted, type Ref } from "vue";
+<script setup lang="ts">
+import { ref, onMounted, type Ref } from "vue";
 
-import BsItemHome from "./components/BsItemHome.vue";
+import BsModal from "@/components/BsModal.vue";
+import ModalPutBook from "./components/ModalPutBook.vue";
+
 
 import LivrosApi, { type Livros } from "@/api/livros";
-export default defineComponent({
-  name: "bs-home-view",
-  components: {
-    BsItemHome,
-  },
-  setup() {
-    const livros: Ref<Livros[]> = ref([]);
 
-    const livrosApi: LivrosApi = new LivrosApi();
+const livros: Ref<Livros[]> = ref([]);
+const modalOpened = ref(false);
 
-    const fetchLivros = async () => {
-      livros.value = await livrosApi.getBooks();
-    };
+const livrosApi: LivrosApi = new LivrosApi();
 
-    onMounted(fetchLivros);
+const fetchLivros = async () => {
+  livros.value = await livrosApi.getBooks();
+};
 
-    return {
-      livros,
-    };
-  },
-});
+onMounted(fetchLivros);
+
+function modalClose() {
+  modalOpened.value = false;
+}
 </script>
 
 <style scoped>
--- src/views/components/ModalPutBook.vue
+++ src/views/components/ModalPutBook.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="row cw-modal-information"></div>
+  <div class="row cw-modal-information">
+    <div class="mb-5">
+      <div class="d-flex flex-column mb-4">
+        <label for="bookName" class="mb-1">Nome do livro</label>
+        <input type="bookName" placeholder="Alterar Nome do Livro" />
+      </div>
+      <div class="d-flex flex-column mb-4">
+        <label for="nacionality" class="mb-1">Editora</label>
+        <input type="nacionality" placeholder="Alterar Editora" />
+      </div>
+      <div class="d-flex flex-column">
+        <label for="page" class="mb-1">Páginas</label>
+        <input type="page" placeholder="Alterar quantidade de Páginas" />
+      </div>
+    </div>
+    <button type="button" class="btn btn-success">Alterar</button>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
