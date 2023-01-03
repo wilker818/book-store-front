0eacc573efcb79ce76095ca5b674961067959535
wilker818 <wilkerferreira818@gmail.com>
2023-01-03 12:02:03 -0300

    chore(bookView): adjusted setup composition API

--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -8,34 +8,26 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, onMounted, type Ref } from "vue";
+<script setup lang="ts">
+import { ref, onMounted, type Ref } from "vue";
 import { useRoute } from "vue-router";
 
 import LivrosApi, { type Livros } from "@/api/livros";
 
-export default defineComponent({
-  name: "bs-home-view",
-  components: {},
-  setup() {
-    const route = useRoute();
-    const getParamsId: string = route.params.id as string;
+const route = useRoute();
+const getParamsId: string = route.params.id as string;
 
-    const getLivro: Ref<Livros[]> = ref([]);
+const getLivro: Ref<Livros[]> = ref([]);
 
-    const livrosApi: LivrosApi = new LivrosApi();
+const livrosApi: LivrosApi = new LivrosApi();
 
-    const fetchLivro = async () => {
-      getLivro.value = await livrosApi.getBook(getParamsId);
-    };
+const fetchLivro = async () => {
+  getLivro.value = await livrosApi.getBook(getParamsId);
+};
 
-    onMounted(fetchLivro);
+console.log(getLivro.value);
 
-    return {
-      getLivro,
-    };
-  },
-});
+onMounted(fetchLivro);
 </script>
 
 <style scoped>
