7aa1adc4b8d77fbe1b2d02e5061681b54cfc11c9
wilker818 <wilkerferreira818@gmail.com>
2023-01-05 16:38:35 -0300

    feat(api/autores): adjusted method post to creat author

--- src/api/autores/index.ts
+++ src/api/autores/index.ts
@@ -10,6 +10,17 @@ class AutoresApi extends Api {
   async get(): Promise<Autor[]> {
     return (await this.axios.get<Autor[]>("/autores")).data;
   }
+  async getAuthor(params: string): Promise<Autor[]> {
+    return (await this.axios.get<Autor[]>(`/autores/${params}`)).data;
+  }
+
+  async putAuthor(params: string, nome?: string, nacionalidade?: string): Promise<Autor[]> {
+    return (await this.axios.put<Autor[]>(`/autores/${params}`, {nome: nome , nacionalidade: nacionalidade})).data;
+  }
+
+  async postAuthor(nome?: string, nacionalidade?: string): Promise<Autor[]> {
+    return (await this.axios.post<Autor[]>(`/autores`, {nome: nome , nacionalidade: nacionalidade})).data;
+  }
 }
 
 export default AutoresApi;
