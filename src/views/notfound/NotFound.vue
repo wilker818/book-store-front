1a4613e7ec1ab789ed416596167e3aa4e5fea7bd
wilker818 <wilkerferreira818@gmail.com>
2023-01-09 10:19:35 -0300

    chore(not-found): adjusted page not found

--- src/views/notfound/NotFound.vue
+++ src/views/notfound/NotFound.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="about">Não ENCONTRADO</div>
+  <div class="d-flex justify-content-center">
+    <div class="bs-not-found">
+      <img src="@/assets/images/not-found.png" alt="Not Found" />
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style scoped>
+.bs-not-found {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+}
+</style>
